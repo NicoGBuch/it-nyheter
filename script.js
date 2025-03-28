9b626f9e73a39397f817c2e47b6b94d376fc97bd
JoGoRef <johannes.refsnes@gmail.com>
2025-03-28 11:02:27 +0100

    fiksa karusell

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function scrollKarusell(direction) {
-    const karusell = document.getElementById('karusell');
+    const karusell = document.getElementById('scroll-karusell');
     const scrollAmount = 310; // Justerer hvor mye det ruller per klikk
     karusell.scrollBy({ left: direction * scrollAmount, behavior: 'smooth' });
 }
